dbd3d5984cab315247e65a0e2d7b44b1327729f2
Lorenzo Kappeler <me@lnk.codes>
2018-02-09 14:37:23 +0100

    fix(cache): Fix the cache handling

--- dummy-service-one/app/index.js
+++ dummy-service-one/app/index.js
@@ -6,7 +6,7 @@ app.listen(10201);
 
 app.get('/', function (req, res) {
   res.setHeader("Vary", "User-Agent");
-  res.setHeader('Last-Modified', (new Date(2018,0,1)).toUTCString());
+  //res.setHeader('Last-Modified', (new Date(2018,0,1)).toUTCString());
   // 86400 = 1d
   res.setHeader("Cache-Control", "public, max-age=86400");
 
