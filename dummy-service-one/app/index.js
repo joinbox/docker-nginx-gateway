0241d995ff74b3f878ced40ef9b2639168a65dec
Lorenzo Kappeler <me@lnk.codes>
2018-02-09 10:39:07 +0100

    feat(dummy-service-one): Add cahce headers

--- dummy-service-one/app/index.js
+++ dummy-service-one/app/index.js
@@ -1,8 +1,14 @@
 var express = require('express')
 var app = express()
 
+app.set('etag', false);
 app.listen(10201);
 
 app.get('/', function (req, res) {
-  res.send('dummy service one')
+  res.setHeader("Vary", "User-Agent");
+  res.setHeader('Last-Modified', (new Date(2018,0,1)).toUTCString());
+  // 86400 = 1d
+  res.setHeader("Cache-Control", "public, max-age=86400");
+
+  res.send(`Created at ${new Date(Date.now()).toUTCString()}`)
 })
